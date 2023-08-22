f1ca416ad8135ca6c4157d5845470021f69814a3
Ashish <ash.ishu025@gmail.com>
2023-08-22 22:45:46 +0530

    restful apis using express

---
+++ ES6/api.js
@@ -0,0 +1,20 @@
+//http / https :=> hypertext type protocol
+//html => hypertext markup language
+
+//http - stateless
+
+//we need to save information on server so that we can access anywhere
+
+//user - 
+//https://www.arit.userbase.com/api/user/searches - {search items} - post
+
+//https://www.arit.userbase.com/api/user/get/searches/userid - get
+
+//saving information on ui/screen - representation
+
+//REST - Respresentational State Transfer Protocol
+
+//Rest ful API's have below specifications -
+//it uses http
+//definitive verbs or http method - get/put/post/patch/delete
+//stateless
