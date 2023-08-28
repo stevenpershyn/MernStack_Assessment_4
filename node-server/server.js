5ca4dc9d1b9e107299b4ce747cc5123116e251e9
Ashish <ash.ishu025@gmail.com>
2023-08-28 22:50:12 +0530

    req response queryparam routeparam

--- node-server/server.js
+++ node-server/server.js
@@ -7,12 +7,39 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// query param
+app.get('/queryparam', function (req, res) {
+  let query = req.query["name"]
+
+  res.send(`This is the name sent in queery ${query}`)
+})
+
+// route param :/name - of the param //http://localhost:9000/routeprm/Jeffery/info
+app.get('/routeprm/:name/info', function (req, res) {
+  let routeParam = req.params["name"]
+
+  res.send(`This is the name sent in Route param ${routeParam}`)
+})
+
 app.get('/helloapi', function (req, res) {
-    res.json({
-        "Name":"Jason",
-        "Status" : 2000,
-        "Session" : "MERNStack"
+    console.log(req.headers) 
+  
+  let deviceType = req.headers['user-agent']
+  console.log(deviceType) 
+  if (deviceType.indexOf("Android") >= 0) {
+      res.json({
+        "Device":"Mobile",
+        "Status" : 102,
+        "Message" : "Please switch off during session!!!"
     })
+  } else {
+    res.json({
+      "Name":"Jason",
+      "Status" : 2000,
+      "Session" : "MERNStack"
+  }) 
+  }
+   
 })
 
 
