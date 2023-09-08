3bdedeb46b0df44ce7ade8612c9b5a460dcff137
Ashish <ash.ishu025@gmail.com>
2023-09-08 22:47:08 +0530

    react start

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -4,16 +4,27 @@ import Header from "./CommonComponent/HeaderComponent";
 
 export default class Application extends Component {
         constructor(props){
-            super(props)    
+            super(props)  
+            this.state = {
+                headerValue : "My First React Application Page"
+            }  
+        }
+
+        changeText = ()=>{
+            this.setState({
+                headerValue : "New Title MERNStack"
+            })
         }
 
         render(){
             let name = "Van Duc Phan"
+            console.log("Creating virtual dom")
             //JSX - code, html+javascript creating a virtual dom
             return( 
                 <>
                     <Header />
-                    <h1>My First React Application Page</h1>   
+                    <h1>{this.state.headerValue}</h1>   
+                    <button onClick={this.changeText}>Change Text</button>
                     <h2> Hello React {name} </h2>
                 </>
             )
