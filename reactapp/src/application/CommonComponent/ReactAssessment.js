c05376bb003419ad89fc0007ce59cfecf5c8169f
Ashish <ash.ishu025@gmail.com>
2023-09-15 21:51:30 +0530

    assessment #3

---
+++ reactapp/src/application/CommonComponent/ReactAssessment.js
@@ -0,0 +1,12 @@
+//Assessment #3 react and its fundamentals - 15/09/2023
+
+//1. what is react and its role for single page application
+//2. how react is faster compared to other javascript frameworks
+//3. what is virtual dom
+//4. how react renders dom in conservative manner
+//5. create a class component named - Success and show some quotes on success in its
+//6. create a functional component SuccessChild, make it child of Success and pass Name and Address to it from Success
+//7. create SuccessStory as another component, pass this as props in SuccessChild from Success component
+//8. create UserSignIn component using uncontrolled way, should be class component
+//9. create Product component to accept ProductName and ProductPrice
+//10. pass a random value from SuccessStory component back to Success
