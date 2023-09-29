867bf16e02fd8d59709259eeea1a19461931d67f
stevenpershyn <102390494+stevenpershyn@users.noreply.github.com>
2023-09-29 15:51:39 -0400

    Update actionTypes.js

--- reactapp/src/state/actionTypes.js
+++ reactapp/src/state/actionTypes.js
@@ -1,3 +1,4 @@
 //This file defines all the action types to be used in various actions and reducers
 
 export const AddUserToStore = "USER.ADDUSERTOSTORE";
+export const AddProductToStore = "PRODUCT.ADDPRODUCTTOSTORE";
