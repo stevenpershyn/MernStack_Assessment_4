b98696b07f72bf4a908ae382e3dd94debeb13df5
Ashish <ash.ishu025@gmail.com>
2023-09-29 21:37:59 +0530

    assessment 4

---
+++ reactapp/src/application/CommonComponent/ReactReduxAssessment.js
@@ -0,0 +1,9 @@
+//Assessment #4
+//1. Create a class component and show all the life cycle hooks
+//2. Make a state change, the state should be duplicate and then stop it to call render method to improve efficiency.
+//3. Create a product component using functional component and hooks (try using use state to read default values from reducer)
+// 	 Create a form to allow user to submit Product Details - name, price, desc, rating
+// 	 Create an action method to add the detail to database using a server api
+// 	 Server Side - Create product router and api to save the product using productdatamodel
+//4. Create a route and navigation to show product component
+//5. Explain various hooks in functional component (at least 5)
