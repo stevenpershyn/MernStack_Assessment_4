1717fc033c537bef4084cf2661125a279a070c14
stevenpershyn <102390494+stevenpershyn@users.noreply.github.com>
2023-09-29 15:40:37 -0400

    Add files via upload

---
+++ node-server/router/product_route.js
@@ -0,0 +1,34 @@
+let express = require("express");
+let productRoute = express.Router();
+const productDataModel = require("../data-model/productDataModel");
+
+productRout.post("/api/signinup", (req, res)=>{
+    let product = req.body
+
+    productDataModel.findOne({name:req.body.name}).then((existingProduct)=>{
+        if(existingProduct) {
+            res.send(existingProduct)
+        } else {
+            let newProduct = new productDataModel(product)
+            newProduct.save().then((newProduct)=>{
+                res.send(newProduct)
+            }).catch((err1)=>{
+                res.send("error while sending")
+            })
+        }
+    }).catch((err)=>{
+        res.send("Error while sending")
+    })
+})
+
+productRoute.get("/api/getproduct",(req, res)=>{
+    productDataModel.find()
+    .then((allusers)=>{
+        res.send(allusers)
+    })
+    .catch(()=>{
+        res.send("error while fetching users")
+    })
+})
+
+module.exports = productRoute;
